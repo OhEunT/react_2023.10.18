5eee7509b027eaf4f8d7c0956270d646c06839d0
OhEunT <gogndnis@gmail.com>
2023-10-18 12:42:03 +0900

    배22포

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -4,7 +4,7 @@ function Visual() {
 
   return (
     <figure id='visual' className="myScroll">
-      {/* <video src={process.env.PUBLIC_URL + "/img/vid.mp4"} loop muted autoPlay></video> */}
+      <video src={process.env.PUBLIC_URL + "/img/vid.mp4"} loop muted autoPlay></video>
       {/* 비디오를 넣을때 기존의 방법으로 하면 비디오가 검은색 화면으로만 출력되거나 이미지만 출력되는 식의 문제가 있습니다 */}
     </figure>
   )
